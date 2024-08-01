de11e233f74a717b8355044aeb960e0b5ee9f1a8
mogoo7zn <znwangking@hotmail.com>
2024-08-01 18:25:07 +0800

    pushed by mogoo

---
+++ WidgetLib/src/main/java/com/example/widgetlib/weekLine.java
@@ -0,0 +1,25 @@
+package com.example.widgetlib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class weekLine extends View {
+    public weekLine(Context context) {
+        super(context);
+    }
+
+    public weekLine(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public weekLine(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public weekLine(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
