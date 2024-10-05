c18cf8915579ef94d27d5af4a2000a7a6c3c4021
LuoHaomin <LuoHaomin@mail.ustc.edu.cn>
2024-10-05 16:33:27 +0800

    commit by luo (Ladybug 问题修复)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ import com.android.build.api.dsl.Packaging
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.android) version "2.0.0"
     alias(libs.plugins.jetbrains.kotlin.kapt)
     id("com.google.devtools.ksp") version "2.0.0-1.0.23"
     id("androidx.room") version "2.6.1" apply false
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) version "2.0.0" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.23"
     alias(libs.plugins.jetbrains.kotlin.kapt) apply false
     id("androidx.room") version "2.6.1" apply false
